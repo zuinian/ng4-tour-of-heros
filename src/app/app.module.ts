431d7364e170af962426bf1679b370530424f862
zuinian <1049967814@qq.com>
2017-08-31 15:57:02 +0800

    完成3. Multiple components 学习：创建对象文件，子组件，包含模板

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent }  from './app.component';
-
+import { HeroDetailComponent } from './hero-detail.component';
 @NgModule({
   imports:      [
       BrowserModule,
       FormsModule // <-- import the FormsModule before binding with [(ngModel)]
   ],
-  declarations: [ AppComponent
+  declarations: [
+      AppComponent,
+      HeroDetailComponent
   ],
   bootstrap:    [ AppComponent ]
 })
