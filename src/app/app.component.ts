14fa90bb60650b4605ee178eb3697bb2ea55ec57
zuinian <1049967814@qq.com>
2017-08-31 17:16:25 +0800

    完成5. Services 学习：服务，Oninit类继承与初始化，Promise获取数据（让异步变同步）

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
-
+import { HeroService } from './hero.service';
 @Component({
     selector: 'my-app',
     template: `
@@ -75,13 +62,18 @@ const HEROES: Hero[] = [
             margin-right: .8em;
             border-radius: 4px 0 0 4px;
         }
-    `]
+    `],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     title = 'Tour of Heros';
-    heroes = HEROES;
+    heroes: Hero[];
     selectedHero: Hero;
 
+    constructor(private heroService: HeroService) {}
+    ngOnInit(): void {
+        this.heroService.getHeroesSlowly().then(heros => this.heroes = heros);
+    }
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     }
